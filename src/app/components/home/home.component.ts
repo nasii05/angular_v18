5adf9cf7d43ab0647740e35f64cac94b806081cb
nasii05 <iamnasih@gmail.com>
2024-05-25 22:12:20 +0530

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  userForm!: FormGroup;
+  constructor(private fb: FormBuilder){
+    this.userForm = this.fb.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+        rollNo: ['']
+    })
+  }
+
+
+
+}
